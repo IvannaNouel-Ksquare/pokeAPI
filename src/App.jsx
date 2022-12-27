eb0695ceb2834cd4929792cbf280101105f5c508
Ivanna <ivannamarcelle@gmail.com>
2022-12-27 01:31:21 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import DataDisplayer from './components/Pokebox';
+import DataDisplayer from './components/Pokemons/pokemonsData';
 
 function App() {
   const pokemons=[1,2,3,4,5];
   const List = pokemons.map((data) => <DataDisplayer id={data} key={data}/>);
-  
+
     return (
     <body className="App">
        {List}
